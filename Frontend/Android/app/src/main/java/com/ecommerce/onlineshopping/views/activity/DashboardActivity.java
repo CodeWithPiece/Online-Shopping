b3e0ccd7be7382425725af5a12d6c4e84ce301a9
Nirmal <145927850+CodeWithPiece@users.noreply.github.com>
2023-12-07 18:18:15 +0530

    Adding Bottom Navigation and Fragment

--- Frontend/Android/app/src/main/java/com/ecommerce/onlineshopping/views/activity/DashboardActivity.java
+++ Frontend/Android/app/src/main/java/com/ecommerce/onlineshopping/views/activity/DashboardActivity.java
@@ -2,9 +2,12 @@ package com.ecommerce.onlineshopping.views.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.os.Bundle;
 
 import com.ecommerce.onlineshopping.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class DashboardActivity extends AppCompatActivity {
 
@@ -12,5 +15,11 @@ public class DashboardActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        BottomNavigationView bottomNavigation = findViewById(R.id.bottomNavigation);
+
+        bottomNavigation.setItemActiveIndicatorColor(ColorStateList.valueOf(Color.TRANSPARENT));
+        bottomNavigation.setItemRippleColor(ColorStateList.valueOf(Color.TRANSPARENT));
+
     }
 }
--- Frontend/Android/app/src/main/java/com/ecommerce/onlineshopping/views/activity/LoginActivity.java
+++ Frontend/Android/app/src/main/java/com/ecommerce/onlineshopping/views/activity/LoginActivity.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.widget.TextView;
 
 import com.ecommerce.onlineshopping.R;
+import com.google.android.material.button.MaterialButton;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -17,12 +18,20 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         TextView txtSignUp = findViewById(R.id.txtSignUp);
+        MaterialButton btnLogin = findViewById(R.id.btnLogin);
 
         txtSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(LoginActivity.this, SignUpActivity.class));
                 finish();
+            }
+        });
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, DashboardActivity.class));
             }
         });
 
