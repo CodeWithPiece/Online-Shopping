d23cac5b32d634dc83e3499974e61da0fe536d42
Nirmal <145927850+CodeWithPiece@users.noreply.github.com>
2023-12-08 13:39:18 +0530

    Home Trending Product Added

---
+++ Frontend/Android/app/src/main/java/com/ecommerce/onlineshopping/adapter/TrendingClothesAdapter.java
@@ -0,0 +1,45 @@
+package com.ecommerce.onlineshopping.adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.ecommerce.onlineshopping.R;
+import com.ecommerce.onlineshopping.views.fragment.HomeFragment;
+
+public class TrendingClothesAdapter extends RecyclerView.Adapter<TrendingClothesAdapter.ViewHolder> {
+
+    HomeFragment homeFragment;
+
+    public TrendingClothesAdapter(HomeFragment homeFragment) {
+        this.homeFragment = homeFragment;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.custom_product_item_layout, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 10;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+
+}
--- Frontend/Android/app/src/main/java/com/ecommerce/onlineshopping/views/fragment/HomeFragment.java
+++ Frontend/Android/app/src/main/java/com/ecommerce/onlineshopping/views/fragment/HomeFragment.java
@@ -3,6 +3,8 @@ package com.ecommerce.onlineshopping.views.fragment;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,6 +12,7 @@ import android.view.ViewGroup;
 
 import com.ecommerce.onlineshopping.R;
 import com.ecommerce.onlineshopping.adapter.BannerAdapter;
+import com.ecommerce.onlineshopping.adapter.TrendingClothesAdapter;
 import com.smarteist.autoimageslider.SliderView;
 
 public class HomeFragment extends Fragment {
@@ -25,7 +28,11 @@ public class HomeFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
 
         SliderView slider = view.findViewById(R.id.slider);
+        RecyclerView trendingRecycler = view.findViewById(R.id.trendingRecycler);
         BannerAdapter bannerAdapter = new BannerAdapter(HomeFragment.this);
+        TrendingClothesAdapter trendingClothesAdapter = new TrendingClothesAdapter(HomeFragment.this);
+        trendingRecycler.setLayoutManager(new GridLayoutManager(getContext(), 2));
+        trendingRecycler.setAdapter(trendingClothesAdapter);
         slider.setAutoCycleDirection(SliderView.LAYOUT_DIRECTION_LTR);
         slider.setSliderAdapter(bannerAdapter);
         slider.setScrollTimeInSec(3);
