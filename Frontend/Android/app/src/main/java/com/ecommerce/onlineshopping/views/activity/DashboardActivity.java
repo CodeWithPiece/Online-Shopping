1beae1b88601e08966aaa7dde5d9a3e8df9ccda3
Nirmal <145927850+CodeWithPiece@users.noreply.github.com>
2023-12-08 12:14:04 +0530

    Adding currenlty required Fragments

--- Frontend/Android/app/src/main/java/com/ecommerce/onlineshopping/views/activity/DashboardActivity.java
+++ Frontend/Android/app/src/main/java/com/ecommerce/onlineshopping/views/activity/DashboardActivity.java
@@ -1,15 +1,27 @@
 package com.ecommerce.onlineshopping.views.activity;
 
+import androidx.activity.OnBackPressedCallback;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.content.res.ColorStateList;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.ecommerce.onlineshopping.R;
+import com.ecommerce.onlineshopping.views.fragment.CartFragment;
+import com.ecommerce.onlineshopping.views.fragment.CategoryFragment;
+import com.ecommerce.onlineshopping.views.fragment.FavouriteFragment;
+import com.ecommerce.onlineshopping.views.fragment.HomeFragment;
+import com.ecommerce.onlineshopping.views.fragment.ProfileFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class DashboardActivity extends AppCompatActivity {
+
+    String currentFrag = "HOME";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +32,50 @@ public class DashboardActivity extends AppCompatActivity {
 
         bottomNavigation.setItemActiveIndicatorColor(ColorStateList.valueOf(Color.TRANSPARENT));
         bottomNavigation.setItemRippleColor(ColorStateList.valueOf(Color.TRANSPARENT));
+        loadFragment(new HomeFragment());
+        bottomNavigation.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if (item.getItemId() == R.id.home) {
+                    currentFrag = "HOME";
+                    loadFragment(new HomeFragment());
+                } else if (item.getItemId() == R.id.category) {
+                    currentFrag = "CATEGORY";
+                    loadFragment(new CategoryFragment());
+                } else if (item.getItemId() == R.id.favourite) {
+                    currentFrag = "FAVOURITE";
+                    loadFragment(new FavouriteFragment());
+                } else if (item.getItemId() == R.id.cart) {
+                    currentFrag = "CART";
+                    loadFragment(new CartFragment());
+                } else {
+                    currentFrag = "PROFILE";
+                    loadFragment(new ProfileFragment());
+                }
+                return true;
+            }
+        });
 
+        getOnBackPressedDispatcher().addCallback(new OnBackPressedCallback(true) {
+            @Override
+            public void handleOnBackPressed() {
+                if (currentFrag.equals("HOME")) {
+                    finish();
+                } else {
+                    currentFrag = "HOME";
+                    loadFragment(new HomeFragment());
+                    bottomNavigation.setSelectedItemId(R.id.home);
+                    bottomNavigation.setSelected(true);
+                }
+            }
+        });
+
+    }
+
+    public void loadFragment(Fragment fragment) {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.container, fragment)
+                .commit();
     }
 }
