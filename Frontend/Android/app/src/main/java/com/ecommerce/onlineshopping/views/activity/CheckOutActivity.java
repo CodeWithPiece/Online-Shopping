d518d5e244098360b72327f2524f412d36ff5147
Nirmal <145927850+CodeWithPiece@users.noreply.github.com>
2023-12-09 13:05:47 +0530

    Check out section and order placed section done

--- Frontend/Android/app/src/main/java/com/ecommerce/onlineshopping/views/activity/CheckOutActivity.java
+++ Frontend/Android/app/src/main/java/com/ecommerce/onlineshopping/views/activity/CheckOutActivity.java
@@ -2,11 +2,13 @@ package com.ecommerce.onlineshopping.views.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
 import com.ecommerce.onlineshopping.R;
 import com.google.android.material.appbar.MaterialToolbar;
+import com.google.android.material.button.MaterialButton;
 
 public class CheckOutActivity extends AppCompatActivity {
 
@@ -16,11 +18,21 @@ public class CheckOutActivity extends AppCompatActivity {
         setContentView(R.layout.activity_check_out);
 
         MaterialToolbar toolBar = findViewById(R.id.toolBar);
+        MaterialButton btnOrder = findViewById(R.id.btnOrder);
 
         toolBar.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 CheckOutActivity.super.onBackPressed();
+            }
+        });
+
+        btnOrder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(CheckOutActivity.this, OrderPlacedActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
             }
         });
 
--- Frontend/Android/app/src/main/java/com/ecommerce/onlineshopping/views/activity/OrderPlacedActivity.java
+++ Frontend/Android/app/src/main/java/com/ecommerce/onlineshopping/views/activity/OrderPlacedActivity.java
@@ -1,16 +1,87 @@
 package com.ecommerce.onlineshopping.views.activity;
 
+import androidx.activity.OnBackPressedCallback;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.View;
 
 import com.ecommerce.onlineshopping.R;
+import com.google.android.material.button.MaterialButton;
+
+import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
+
+import nl.dionsegijn.konfetti.core.Angle;
+import nl.dionsegijn.konfetti.core.PartyFactory;
+import nl.dionsegijn.konfetti.core.Position;
+import nl.dionsegijn.konfetti.core.Spread;
+import nl.dionsegijn.konfetti.core.emitter.Emitter;
+import nl.dionsegijn.konfetti.core.emitter.EmitterConfig;
+import nl.dionsegijn.konfetti.core.models.Shape;
+import nl.dionsegijn.konfetti.xml.KonfettiView;
 
 public class OrderPlacedActivity extends AppCompatActivity {
+
+    private KonfettiView konfettiView = null;
+    private Shape.DrawableShape drawableShape = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order_placed);
+        getWindow().setStatusBarColor(ContextCompat.getColor(OrderPlacedActivity.this, R.color.green));
+
+        final Drawable drawable = ContextCompat.getDrawable(getApplicationContext(), R.drawable.heart_filled);
+        drawableShape = new Shape.DrawableShape(drawable, true, true);
+        konfettiView = findViewById(R.id.konfettiView);
+        MaterialButton btnViewOrder = findViewById(R.id.btnViewOrder);
+
+        btnViewOrder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(OrderPlacedActivity.this, MyOrderActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+            }
+        });
+
+        parade();
+
     }
+
+    public void parade() {
+        EmitterConfig emitterConfig = new Emitter(1, TimeUnit.SECONDS).perSecond(100);
+        konfettiView.start(
+                new PartyFactory(emitterConfig)
+                        .angle(Angle.RIGHT - 45)
+                        .spread(Spread.WIDE)
+                        .shapes(Arrays.asList(Shape.Square.INSTANCE, Shape.Circle.INSTANCE, drawableShape))
+                        .colors(Arrays.asList(0xfce18a, 0xff726d, 0xf4306d, 0xb48def))
+                        .setSpeedBetween(10f, 30f)
+                        .position(new Position.Relative(0.0, 0.3))
+                        .build(),
+                new PartyFactory(emitterConfig)
+                        .angle(Angle.LEFT + 45)
+                        .spread(Spread.WIDE)
+                        .shapes(Arrays.asList(Shape.Square.INSTANCE, Shape.Circle.INSTANCE, drawableShape))
+                        .colors(Arrays.asList(0xfce18a, 0xff726d, 0xf4306d, 0xb48def))
+                        .setSpeedBetween(10f, 30f)
+                        .position(new Position.Relative(1.0, 0.3))
+                        .build()
+        );
+
+        getOnBackPressedDispatcher().addCallback(new OnBackPressedCallback(true) {
+            @Override
+            public void handleOnBackPressed() {
+                startActivity(new Intent(OrderPlacedActivity.this, DashboardActivity.class));
+                finishAffinity();
+            }
+        });
+
+    }
+
 }
