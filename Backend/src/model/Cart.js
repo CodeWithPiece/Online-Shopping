7a400b29ff9de3f8941b812718111daac2157ddd
Nirmal <145927850+CodeWithPiece@users.noreply.github.com>
2023-12-16 23:46:06 +0530

    Update Cart Api added

--- Backend/src/model/Cart.js
+++ Backend/src/model/Cart.js
@@ -25,17 +25,35 @@ Cart.addToCart = (m, result) => {
 };
 
 Cart.updateCart = (m, result) => {
-    connection.query(
-      "UPDATE online_shopping.cart SET productCount=?, updatedAt=? WHERE cartId=?",
-      [m.productCount, m.updatedAt, m.cartId],
-      (err, res) => {
-        if (err) {
-          return result(err, null);
-        } else {
+  connection.query(
+    "UPDATE online_shopping.cart SET productCount=?, updatedAt=? WHERE cartId=?",
+    [m.productCount, m.updatedAt, m.cartId],
+    (err, res) => {
+      if (err) {
+        return result(err, null);
+      } else {
+        return result(null, res);
+      }
+    }
+  );
+};
+
+Cart.getCartByUserId = (userId, result) => {
+  connection.query(
+    "SELECT online_shopping.product_size.sizeId, online_shopping.product_size.sizeName, online_shopping.product_size.productId, online_shopping.product_size.updatedAt, online_shopping.product_size.createdAt FROM online_shopping.product_size JOIN online_shopping.product ON online_shopping.product_size.productId = online_shopping.product.productId WHERE online_shopping.product_size.productId=?",
+    [productId],
+    (err, res) => {
+      if (err) {
+        return result(err, null);
+      } else {
+        if (res && res.length) {
           return result(null, res);
+        } else {
+          return result(null, []);
         }
       }
-    );
-  };
+    }
+  );
+};
 
 module.exports = Cart;
