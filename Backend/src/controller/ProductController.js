9eba48efce1ee26a30d51dad3ed924d84d20e6fa
Nirmal <145927850+CodeWithPiece@users.noreply.github.com>
2023-12-16 17:38:52 +0530

    Removing Error

--- Backend/src/controller/ProductController.js
+++ Backend/src/controller/ProductController.js
@@ -23,7 +23,6 @@ exports.saveProduct = (req, res) => {
 
       productModel.saveProduct(m, (err, product) => {
         if (err) {
-          console.log(err);
           return res.status(500).json({
             status: false,
             message: "Internal server error",
