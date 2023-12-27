65423d061c6fddc07d254440e53b713bc3d6efbe
Nirmal <145927850+CodeWithPiece@users.noreply.github.com>
2023-12-27 09:31:48 +0530

    Port updated

--- Frontend/Android/app/src/main/java/com/ecommerce/onlineshopping/utils/Constant.java
+++ Frontend/Android/app/src/main/java/com/ecommerce/onlineshopping/utils/Constant.java
@@ -2,7 +2,7 @@ package com.ecommerce.onlineshopping.utils;
 
 public class Constant {
 
-    public static final String BASE_URL = "http://192.168.106.79:3000/online_shopping/api/v1/";
-    public static final String IMAGE_URL = "http://192.168.106.79:3000";
+    public static final String BASE_URL = "http://192.168.94.185:3000/online_shopping/api/v1/";
+    public static final String IMAGE_URL = "http://192.168.94.185:3000";
 
 }
